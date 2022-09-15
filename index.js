c1c95dfd435eae837eb04fca6fa0085729403d18
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-09-15 12:30:30 -0400

    tgrt

---
+++ index.js
@@ -0,0 +1,50 @@
+const location = [
+  {
+    name: "Mountain Cave",
+    id: "lucky",
+  },
+  {
+    name: "Mysterious Shack",
+    id: "lucky",
+  },
+  {
+    name: "Shabby Hut",
+    id: "lucky",
+  },
+  {
+    name: "Frozen Tundra",
+    id: "lucky",
+  },
+  {
+    name: "Empty Dragon's den",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+  {
+    name: "gloomy",
+    id: "lucky",
+  },
+];
