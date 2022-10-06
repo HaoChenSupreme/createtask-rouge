937fcd2f60cf6d12a0b8269dc8048fa62e287756
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2022-10-06 17:04:38 -0400

    big changes

--- index.js
+++ index.js
@@ -79,13 +79,93 @@ const locations = [
     value: "-1",
     type: "unlucky",
   },
+  {
+    name: "Pirate Cove",
+    value: "1",
+    type: "lucky",
+  },
+  {
+    name: "Port City",
+    value: "2",
+    type: "lucky",
+  },
+  {
+    name: "Town Square",
+    value: "3",
+    type: "lucky",
+  },
+  {
+    name: "Empty City",
+    value: "2",
+    type: "lucky",
+  },
+  {
+    name: "Garden Of Eden",
+    value: "5",
+    type: "lucky",
+  },
+  {
+    name: "Wishing Well",
+    value: "3",
+    type: "lucky",
+  },
+  {
+    name: "Sea God's Palace",
+    value: "4",
+    type: "lucky",
+  },
+  {
+    name: "Oakwood Graveyard",
+    value: "1",
+    type: "lucky",
+  },
+  {
+    name: "Stone Forest",
+    value: "-2",
+    type: "unlucky",
+  },
+  {
+    name: "World Tree- Mirimark",
+    value: "-3",
+    type: "unlucky",
+  },
+  {
+    name: "Gambler's Den",
+    value: "-2",
+    type: "unlucky",
+  },
+  {
+    name: "Sea God's Vault",
+    value: "-4",
+    type: "unlucky",
+  },
+  {
+    name: "Old Watchtower",
+    value: "-3",
+    type: "unlucky",
+  },
+  {
+    name: "World Border",
+    value: "-5",
+    type: "unlucky",
+  },
+  {
+    name: "Sea God's Tomb",
+    value: "-1",
+    type: "unlucky",
+  },
+  {
+    name: "Central Market",
+    value: "-1",
+    type: "unlucky",
+  },
 ];
-
+ 
 let leftplace = null;
 let lefthp = null;
 let rightplace = null;
 let righthp = null;
-
+ 
 function randomlocation(locations, n) {
   const shuffled = [...locations].sort(() => Math.random() - Math.random());
   const resultleft = shuffled.slice(0, n - 1);
@@ -105,7 +185,7 @@ function randomlocation(locations, n) {
   console.log(resultleft);
   console.log(resultright);
 }
-
+ 
 const DOMSelectors = {
   leftbtn: document.getElementById("left-btn"),
   rightbtn: document.getElementById("right-btn"),
@@ -114,66 +194,90 @@ const DOMSelectors = {
   hpvalue: document.getElementById("hp"),
   leftdisplay: document.getElementById("left"),
   rightdisplay: document.getElementById("right"),
+  rightValue: document.getElementById("right-value"),
+  leftValue: document.getElementById("left-value"),
+  hpContainer: document.getElementById("hphp"),
 };
-
-DOMSelectors.leftbtn.addEventListener("click", function () {
-  const leftna = randomlocation(locations, 2);
-  console.log(leftplace);
-  DOMSelectors.leftname.innerHTML = "";
-  DOMSelectors.leftname.insertAdjacentHTML(
-    "afterbegin",
-    `<h2 id="left-name">${leftplace}</h2>`
-  );
+ 
+console.log(DOMSelectors.hpContainer);
+ 
+let health = 10;
+ 
+function hitLeft() {
+  let damage = +lefthp;
+  health += damage;
+  if (health <= 0) {
+  alert ("You've Died, Try Again");
+  health = 10;
+}
+}
+ 
+function hitRight() {
+  let damage = +righthp;
+  health += damage;
+  if (health <= 0) {
+    alert ("You've Died, Try Again");
+    health = 10;
+  }
+}
+ 
+ 
+ 
+function getNameRight() {
   DOMSelectors.rightname.innerHTML = "";
   DOMSelectors.rightname.insertAdjacentHTML(
     "afterbegin",
     `<h2 id="left-name">${rightplace}</h2>`
   );
-  DOMSelectors.hpvalue.insertAdjacentHTML(
-    "afterbegin",
-    `<h2 id="hp">${lefthp}</h2>`
-  );
-});
-
-DOMSelectors.rightbtn.addEventListener("click", function () {
-  const rightna = randomlocation(locations, 2);
-  console.log(rightna);
-  DOMSelectors.rightname.innerHTML = "";
-  DOMSelectors.rightname.insertAdjacentHTML(
-    "afterbegin",
-    `<h2 id="left-name">${rightplace}</h2>`
-  );
+}
+ 
+function getNameLeft() {
   DOMSelectors.leftname.innerHTML = "";
   DOMSelectors.leftname.insertAdjacentHTML(
     "afterbegin",
     `<h2 id="left-name">${leftplace}</h2>`
   );
-  DOMSelectors.hpvalue.insertAdjacentHTML(
-    "afterbegin",
-    `<h2 id="hp">${righthp}</h2>`
+}
+ 
+function togetherLeft() {
+  hitLeft();
+  randomlocation(locations, 2);
+  getNameLeft();
+  getNameRight();
+  console.log(health);
+ 
+  DOMSelectors.hpContainer.innerHTML = "";
+  DOMSelectors.hpContainer.insertAdjacentHTML(
+    "beforeend",
+    `<h2>HP:${health}</h2>`
+  );
+}
+ 
+function togetherRight() {
+  hitRight();
+  randomlocation(locations, 2);
+  getNameLeft();
+  getNameRight();
+  console.log(health);
+ 
+  DOMSelectors.hpContainer.innerHTML = "";
+  DOMSelectors.hpContainer.insertAdjacentHTML(
+    "beforeend",
+    `<h2>HP:${health}</h2>`
+  );
+}
+ 
+window.addEventListener("load", function () {
+  randomlocation(locations, 2);
+  getNameLeft();
+  getNameRight();
+ 
+  DOMSelectors.hpContainer.insertAdjacentHTML(
+    "beforeend",
+    `<h2>HP:${health}</h2>`
   );
 });
-
-// function shuffleleft(locations, n) {
-//   const shuffled = [...locations].sort(() => Math.random() - Math.random());
-//   const resultleft = shuffled.slice(0, n);
-//   console.log(resultleft);
-// }
-
-// function shuffleright(locations, n) {
-//  const shuffled = [...locations].sort(() => Math.random() - Math.random());
-//  const resultright = shuffled.slice(0, n);
-//  console.log(resultright);
-// }
-
-// DOMSelectors.leftbtn.addEventListener('click', function(){
-//  const leftna = shuffleleft(locations, 2);
-//  console.log(leftna);
-// }
-// );
-
-// DOMSelectors.rightbtn.addEventListener('click', function(){
-//  const rightna = shuffleleft(locations, 2);
-//  console.log(rightna);
-// }
-// );
+ 
+DOMSelectors.leftbtn.addEventListener("click", togetherLeft);
+ 
+DOMSelectors.rightbtn.addEventListener("click", togetherRight);
